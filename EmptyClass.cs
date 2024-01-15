9d8605374dece4934398604472beacc81221f2d3
annjessil <92198450+annjessil@users.noreply.github.com>
2024-01-15 15:24:38 -0800

    Update EmptyClass.cs

--- EmptyClass.cs
+++ EmptyClass.cs
@@ -1,10 +1,41 @@
-﻿using System;
-namespace Application
+﻿using UnityEngine;
+public class Hero : MonoBehaviour
 {
-    public class EmptyClass
+    void Start()
     {
-        public EmptyClass()
-        {
+
+        int score = 20;
+
+        if (score > 18) {
+            Debug.log("You win!");
+        } else {
+            Debug.log("You lose!");
         }
+
+        while (score > 10)
+        {
+            score--;
+        }
+
+        for (int i = 1; i <= 4; i++)
+        {
+            score = score + i;
+        }
+
+        Debug.log(score);
+        /* TODO: create a variable of type int with name 'score', and assign
+        a value of 20 to it */
+        /* TODO: write an if-statement that checks the score (the variable
+        you created above) is greater than 18. If so, print (using
+        Debug.Log()) "You win!", otherwise, print "You lose" 
+
+         * TODO: write a while-loop that checks if the score is greater than
+        10. Inside the loop, decrement the score by 1 */
+        /* TODO: write a for-loop that loops a total of 4 times, starting
+        from 1, and in each loop adds the value of the counter variable
+        to the score */
+        /* TODO: print out the final score to console */
     }
+
+
 }
